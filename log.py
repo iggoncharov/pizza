dc6069665fcded0fa6b24bbeeb83da9a83fa447b
iggoncharov <56764427+iggoncharov@users.noreply.github.com>
2021-12-22 17:52:53 +0300

    Add files via upload

---
+++ log.py
@@ -0,0 +1,46 @@
+from pizza import BasePizza, Margherita, Pepperoni, Hawaiian
+import time
+import random
+from typing import Callable
+from functools import wraps
+
+
+def log(params: str) -> Callable:
+
+    def outer_wrapper(func: Callable) -> Callable:
+
+        def inner_wrapper(pizza: BasePizza) -> str:
+            """
+            Вставляет случайное чилос в шаблон в зависимости от размера
+            """
+
+            if func.__name__ == 'bake':
+                if pizza.size == "XL":
+                    return params.format(random.randint(11, 12))
+                else:
+                    return params.format(random.randint(5, 10))
+
+            elif func.__name__ == 'delivery_':
+                return params.format(random.randint(30, 60))
+
+            elif func.__name__ == 'pickup':
+                return params.format(random.randint(20, 30))
+
+        return inner_wrapper
+
+    return outer_wrapper
+
+
+@log("🍳Готовит пиццу за {} мин ⏳!")
+def bake(pizza: BasePizza) -> int:
+    """Готовит пиццу"""
+
+
+@log("🚚Доставляет пиццу за {} мин ⏳!")
+def delivery_(pizza: BasePizza) -> int:
+    """Доставляет пиццу"""
+
+
+@log("🏃Самовывоз пиццы через {} min ⌛!")
+def pickup(pizza: BasePizza) -> int:
+    """Самовывоз"""
