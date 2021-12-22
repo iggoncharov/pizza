3b8664361f7fb822443718f26dd1b08e671c9126
iggoncharov <56764427+iggoncharov@users.noreply.github.com>
2021-12-22 18:25:08 +0300

    Update test_pizza.py

--- test_pizza.py
+++ test_pizza.py
@@ -9,15 +9,15 @@ from cli import order, menu
 def test_dict():
     """Тестирует свойство ingredient, которое выводит рецепт"""
     assert Hawaiian(size="L").ingredient == {
-        "🥫 tomato sauce": 50,
-        "🧀 mozzarella": 100,
-        "🍗 chicken": 50,
-        "🍍 pineapples": 50,
+        '🥫 tomato sauce': 50,
+        '🧀 mozzarella': 100,
+        '🍗 chicken': 50,
+        '🍍 pineapples': 50,
     }
     assert Pepperoni(size="XL").ingredient == {
-        "🥫 tomato sauce": 75,
-        "🧀 mozzarella": 150,
-        "🍪 pepperoni": 105,
+        '🥫 tomato sauce': 75,
+        '🧀 mozzarella': 150,
+        '🍪 pepperoni': 75,
     }
     assert "🍅 tomatoes" in Margherita().ingredient
 
@@ -35,7 +35,7 @@ def test_bake():
     my_number = 4
     with patch.object(random, "randint", return_value = my_number):
         assert (
-            bake(Pepperoni()) == "🍳Готовит пиццу за 4 мин ⏳!"
+            bake(Pepperoni()) == '🍳Готовит пиццу за 4 мин ⏳!'
         )
 
 
@@ -44,7 +44,7 @@ def test_delivery_():
     my_number = 4
     with patch.object(random, "randint", return_value = my_number):
         assert (
-            delivery_(Pepperoni()) == "🚚Доставляет пиццу за 4 мин ⏳!"
+            delivery_(Pepperoni()) == '🚚Доставляет пиццу за 4 мин ⏳!'
         )
 
 
@@ -53,7 +53,7 @@ def test_pickup():
     my_number = 4
     with patch.object(random, "randint", return_value = my_number):
         assert (
-            pickup(Pepperoni()) == "🏃Самовывоз пиццы через 4 min ⌛!"
+            pickup(Pepperoni()) == '🏃Самовывоз пиццы через 4 min ⌛!'
         )
 
 
@@ -65,10 +65,10 @@ def test_menu():
         result.output == 'Меню: \n' +
         'Можно выбрать один из двух размеров: \n' +
         'Средний - "L" или Большой - "XL"\n' +
-        "\n" +
-        "Пиццы: \n" +
-        "Margherita 🧀: 🥫 tomato sauce 🧀 mozzarella 🍅 tomatoes\n" +
-        "Hawaiian 🍍: 🥫 tomato sauce 🧀 mozzarella 🍗 chicken " +
-        "🍍 pineapples\n" +
-        "Pepperoni 🍕: 🥫 tomato sauce 🧀 mozzarella 🍪 pepperoni\n"
+        '\n' +
+        'Пиццы: \n' +
+        'Margherita 🧀: 🥫 tomato sauce 🧀 mozzarella 🍅 tomatoes\n' +
+        'Hawaiian 🍍: 🥫 tomato sauce 🧀 mozzarella 🍗 chicken ' +
+        '🍍 pineapples\n' +
+        'Pepperoni 🍕: 🥫 tomato sauce 🧀 mozzarella 🍪 pepperoni\n'
     )
